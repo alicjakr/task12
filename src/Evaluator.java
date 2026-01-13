a445d9514a958a91ee9afede24be278337c2a93d
alicjakr <krupczynskaalicja@gmail.com>
2026-01-13 14:37:40 +0100

    reads file line by line and evals, should throw exceptions

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,13 +1,21 @@
 import java.io.*;
 public class Evaluator {
     public static void main(String[] args) throws Exception {
-        Process process = new ProcessBuilder(
-                "python3",
-                "-c",
-                "print(eval('1+(2*3)'))"
-        ).start();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            System.out.println(reader.readLine());
+
+        File file = new File("/Users/alicjakrupczynska/IdeaProjects/task12/src/example.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                //delete '=' from each equation
+                line=line.substring(0, line.length()-1);
+                String command="print(eval('"+line+"'))";
+
+                //evaluating the equations - used python3 since it works on MacOS
+                Process process = new ProcessBuilder("python3", "-c", command).start();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                    System.out.println(reader.readLine());
+                }
+            }
         }
     }
 }
